1dafc23f4b4bf8d0954fe4aae5a86f6d94173f9f
Weslley Capelari <weslley.capelari@gmail.com>
2022-11-06 12:15:00 -0300

    :sparkles: feat: Added units in Project

--- samples/VCLSample/VCL.dpr
+++ samples/VCLSample/VCL.dpr
@@ -5,7 +5,12 @@ uses
   MainForm in 'MainForm.pas' {FMainForm},
   CowORM.Commons in '..\..\src\CowORM.Commons.pas',
   CowORM.Core.QueryBuilder in '..\..\src\CowORM.Core.QueryBuilder.pas',
-  CowORM.Interfaces in '..\..\src\CowORM.Interfaces.pas';
+  CowORM.Interfaces in '..\..\src\CowORM.Interfaces.pas',
+  CowORM.Core.Columns in '..\..\src\CowORM.Core.Columns.pas',
+  CowORM.Constants in '..\..\src\CowORM.Constants.pas',
+  CowORM.Core.Tables in '..\..\src\CowORM.Core.Tables.pas',
+  CowORM.Helpers in '..\..\src\CowORM.Helpers.pas',
+  CowORM.Core.QueryCondition in '..\..\src\CowORM.Core.QueryCondition.pas';
 
 {$R *.res}
 
